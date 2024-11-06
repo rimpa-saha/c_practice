6f7d7f2a33667a2d9b26bbe5012d32c34c320287
rimpa-saha <rimpasaha0123@gmail.com>
2024-11-06 15:55:32 +0530

    easy question

--- question1.c
+++ question1.c
@@ -1,14 +1,7 @@
 #include<stdio.h>
 int main(){
-    int n;
-printf("enter a number:");
-scanf("%d",&n);
-for(int i=2;i<=n-1;i=i+1){
-    if(n%2==0){
-        printf("the given nuber is composite");
-        break;
-    }
+for(int i=1;i<=10;i++){
+    printf("%d ",i);
 }
-
     return 0;
 }
